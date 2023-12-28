478cefddb83e043e25c4734f1419af55b5255044
josias.m.caitano <josiasmartinscaitano@gmail.com>
2023-12-28 12:07:42 -0300

    feature: add other methods in LoggerUtil

--- src/main/java/com/jonca/libjava/utils/LoggerUtil.java
+++ src/main/java/com/jonca/libjava/utils/LoggerUtil.java
@@ -7,9 +7,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LoggerUtil {
 
@@ -29,6 +27,13 @@ public class LoggerUtil {
         return  propertiesMap;
     }
 
+    /**
+     * Recursively extracts properties from an object and populates the property map.
+     *
+     * @param object         The object to extract properties from.
+     * @param prefix         The prefix for the property name.
+     * @param propertiesMap  The map to store extracted properties
+     */
     private static void extractPropertiesRecursively(Object object, String prefix, Map<String, String> propertiesMap) {
         if (object == null) {
             return;
@@ -51,6 +56,11 @@ public class LoggerUtil {
                         propertiesMap.put(propertyName, value != null ? value.toString() : "null");
                     } else if (value instanceof List<?>) {
                         processList((List<?>) value, propertyName, propertiesMap);
+                    } else if (isCollectionImmutable(value)) {
+                        Object mutableCopy = createMutableCopy(value);
+                        extractPropertiesRecursively(mutableCopy, propertyName, propertiesMap);
+                    } else {
+                        extractPropertiesRecursively(value, propertyName, propertiesMap);
                     }
 
                 } catch (NoSuchMethodException | SecurityException | IllegalAccessException | InvocationTargetException ex) {
@@ -128,5 +138,37 @@ public class LoggerUtil {
         }
     }
 
+    /**
+     * Concatenates the property name with the index for array or list elements.
+     *
+     * @param propertyName The name of the property
+     * @param index        The index of the array or list elements
+     * @return The concatenated property name
+     */
+    public static String concatFullNameProperty(String propertyName, int index) {
+        return propertyName + "[" + index + "]";
+    }
+
+    /**
+     * Creates a mutable copy of a collections if it is immutable
+     * @param value The collection to copy
+     * @return A mutable copy of the collection
+     */
+    private static Object createMutableCopy(Object value) {
+        if (value instanceof List) {
+            List<?> listImmutable = (List<?>) value;
+            return new ArrayList<>(listImmutable);
+        }
+        return value;
+    }
+
+    /**
+     * Checks if a collection is immutable
+     * @param value The collection to check.
+     * @return True if the collection is immutable, false otherwise.
+     */
+    private static boolean isCollectionImmutable(Object value) {
+        return (value instanceof Collection) && value.getClass().getName().startsWith("java.util.ImmutableCollections$");
+    }
 
 }
